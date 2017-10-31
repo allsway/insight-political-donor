7343647ad0d2e6b6d046c066cd7d65b8c26c927f
allsway <ssinick@calstate.edu>
2017-10-31 14:42:56 -0700

    Cleaned up directory

--- run.sh
+++ run.sh
@@ -2,7 +2,5 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
-<<<<<<< HEAD
-=======
 python3 ./src/calculate_donations.py ./input/indiv_header_file.csv ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt
 
